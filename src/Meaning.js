653b1c5ed6194289c1b033957d59b9b506ca6168
mandi-riegle <mandi.riegle@gmail.com>
2022-06-01 10:08:20 -0700

    Rendered the word meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,9 +10,9 @@ export default function Meaning(props){
                 return (
                     <div key={index}>
                         <p>
-                        {definition.definition}
-                        <br />
-                        <em>{definition.example}</em>
+                            {definition.definition}
+                            <br />
+                            <em>{definition.example}</em>
                         </p>
                     </div>
                 );
