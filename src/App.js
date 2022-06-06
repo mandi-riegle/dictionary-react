5bb456a92cee75fc9dd2e8949bbb390541239066
mandi-riegle <mandi.riegle@gmail.com>
2022-06-06 13:53:18 -0700

    Added footer with info

--- src/App.js
+++ src/App.js
@@ -12,8 +12,31 @@ export default function App() {
       <main>
         <Dictionary defaultkeyword="sunset" />
       </main>
-      <footer className="App-footer">
-        <small>Coded by Amanda Riegle</small>
+      <footer className="footer">
+          This project was coded by{" "}
+          <a
+            href="https://amandariegle.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Amanda Riegle
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/mandi-riegle/dictionary-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://dictionary-app-riegle.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
     </div>
   </div>
